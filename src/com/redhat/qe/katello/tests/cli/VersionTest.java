e6af179d1e9fa50d587db92dbba92874204dd4bb
tkolhar <tazimkolhar@gmail.com>
2012-06-14 17:01:27 +0530

    Version Tests fixed

--- src/com/redhat/qe/katello/tests/cli/VersionTest.java
+++ src/com/redhat/qe/katello/tests/cli/VersionTest.java
@@ -10,10 +10,10 @@ public class VersionTest extends KatelloCliTestScript{
 
 	
 	private SSHCommandResult exec_result;
-	@Test(description = "Version - get the version of the sam server")
+	@Test(description = "Version - get the version of the server")
 	public void test_Version(){
 		KatelloVersion version_obj= new KatelloVersion();
 		exec_result = version_obj.cli_version(); 
-		Assert.assertTrue(exec_result.getExitCode().intValue() == 1, "Check - return code");
+		Assert.assertTrue(exec_result.getExitCode().intValue() == 0, "Check - return code");
 	}
 }
