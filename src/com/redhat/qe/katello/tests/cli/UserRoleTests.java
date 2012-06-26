427d9e290ee7f2e3647594b9a7c868c3766f1188
tkolhar <tazimkolhar@gmail.com>
2012-06-26 15:36:27 +0530

    Modified the ldap tests

--- src/com/redhat/qe/katello/tests/cli/UserRoleTests.java
+++ src/com/redhat/qe/katello/tests/cli/UserRoleTests.java
@@ -1,5 +1,6 @@
 package com.redhat.qe.katello.tests.cli;
 import org.testng.annotations.Test;
+import org.testng.SkipException;
 import com.redhat.qe.auto.testng.Assert;
 import com.redhat.qe.katello.base.KatelloCliDataProvider;
 import com.redhat.qe.katello.base.KatelloCliTestScript;
@@ -8,6 +9,7 @@ import com.redhat.qe.katello.base.obj.KatelloUserRole;
 import com.redhat.qe.tools.SSHCommandResult;
 
 public class UserRoleTests extends KatelloCliTestScript{
+	
 	
 	
 	@Test(description="create User Role", groups = {"headpin-cli"}, 
@@ -89,11 +91,12 @@ public class UserRoleTests extends KatelloCliTestScript{
 	//todo : add ldapgroup/removeldap group
 	
 	@Test(description="Add ldap group to a UserRole", groups = {"headpin-cli"},enabled=true)
-	public void test_add_ldap_group(){
+	public void test_add_ldap_group() {
 	            String uid = KatelloTestScript.getUniqueID();
 	            String user_role_name="user_role-ldap_grp-"+ uid;
 	            String ldap_group = "group-"+uid;
 	            SSHCommandResult res;
+	            int exitCode;
 	            KatelloUserRole user_role = new KatelloUserRole(user_role_name, "User Role Created");
 	            res = user_role.create();
 	            Assert.assertTrue(res.getExitCode().intValue()==0, "Check - return code (user role create)");
@@ -101,12 +104,21 @@ public class UserRoleTests extends KatelloCliTestScript{
 	    				String.format(KatelloUserRole.OUT_CREATE,user_role_name)), 
 	    				"Check - returned output string ("+KatelloUserRole.CMD_CREATE+")");
 	            res = user_role.cli_add_ldap_group(ldap_group);
+	            exitCode = res.getExitCode().intValue();
+	            if (exitCode == 244)
+	            {
+	            	throw (new SkipException("Skip"));
+	            }
+	            else
+	            if(exitCode == 0)
+	            {
 	            Assert.assertTrue(res.getExitCode().intValue() == 0,"Check - return code (user role add ldap group)");
 	            Assert.assertTrue(getOutput(res).contains(
 	    				String.format(KatelloUserRole.OUT_LDAP_ADD,ldap_group,user_role_name)), 
 	    				"Check - returned output string ("+KatelloUserRole.CMD_LDAP_GRP_ADD+")");
 	            res = user_role.cli_info();
 	            Assert.assertTrue(res.getExitCode().intValue() == 0,"Check - return code (user role info)");
+	            }
 	                       	            
 	}  
 	
@@ -117,6 +129,7 @@ public class UserRoleTests extends KatelloCliTestScript{
 	            String user_role_name="user_role-ldap_grp-"+ uid;
 	            String ldap_group = "group-"+uid;
 	            SSHCommandResult res;
+	            int exitCode;
 	            KatelloUserRole user_role = new KatelloUserRole(user_role_name, "User Role Created");
 	            res = user_role.create();
 	            Assert.assertTrue(res.getExitCode().intValue()==0, "Check - return code (user role create)");
@@ -124,6 +137,14 @@ public class UserRoleTests extends KatelloCliTestScript{
 	    				String.format(KatelloUserRole.OUT_CREATE,user_role_name)), 
 	    				"Check - returned output string ("+KatelloUserRole.CMD_CREATE+")");
 	            res = user_role.cli_add_ldap_group(ldap_group);
+	            exitCode = res.getExitCode().intValue();
+	            if (exitCode == 244)
+	            {
+	            	throw (new SkipException("Skip"));
+	            }
+	            else
+	            if(exitCode == 0)
+	            {
 	            Assert.assertTrue(res.getExitCode().intValue() == 0,"Check - return code (user role add ldap group)");
 	            Assert.assertTrue(getOutput(res).contains(
 	    				String.format(KatelloUserRole.OUT_LDAP_ADD,ldap_group,user_role_name)), 
@@ -138,7 +159,7 @@ public class UserRoleTests extends KatelloCliTestScript{
 	            res = user_role.cli_info();
 	            Assert.assertTrue(res.getExitCode().intValue() == 0,"Check - return code (user role info)");
 	                       
-	           	            
+	            }	            
 	}    
 	
 	
