b5024107d36a5a4c6da0604b7aeea50cedeb4db6
Garik Khachikyan <gkhachik@redhat.com>
2012-10-12 16:39:28 +0200

    fixing automerge issue: KatelloUser

--- src/com/redhat/qe/katello/base/obj/KatelloUser.java
+++ src/com/redhat/qe/katello/base/obj/KatelloUser.java
@@ -30,7 +30,7 @@ public class KatelloUser extends _KatelloObject{
 	public static final String OUT_UNASSIGN_ROLE =
 			"User \'%s\' unassigned from role \'%s\'";
 	public static final String OUT_FIND_USER_ERROR =
-			"Could not find user \'%s\'";
+			"Could not find user [ %s ]";
 
 	public static final String REG_USER_LIST = ".*Id\\s*:\\s*\\d+.*Username\\s*:\\s*%s.*Email\\s*:\\s*%s.*";
 	public static final String REG_USER_ROLE_LIST = ".*\\d+\\s*%s.*";
