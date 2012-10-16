45e23c0bd1287d6f739a051a39921d1811b86bc8
Garik Khachikyan <gkhachik@redhat.com>
2012-10-16 18:25:51 +0200

    i18n - provider tests (start)

---
+++ src/com/redhat/qe/katello/tests/i18n/ProviderTests.java
@@ -0,0 +1,34 @@
+package com.redhat.qe.katello.tests.i18n;
+
+import org.testng.annotations.Test;
+
+import com.redhat.qe.Assert;
+import com.redhat.qe.katello.base.KatelloCliTestScript;
+import com.redhat.qe.katello.base.obj.KatelloOrg;
+import com.redhat.qe.katello.base.obj.KatelloProvider;
+import com.redhat.qe.katello.common.KatelloUtils;
+import com.redhat.qe.tools.SSHCommandResult;
+
+public class ProviderTests extends KatelloCliTestScript {
+
+	private String org_name;
+	private String provider_name;
+	private String uid;
+	
+	@Test(description="init - org")
+	public void init(){
+		uid = KatelloUtils.getUniqueID();
+		org_name = getText("org.create.name")+" "+uid;
+		KatelloOrg org = new KatelloOrg(org_name, null);
+		SSHCommandResult res = org.cli_create();
+		Assert.assertTrue(res.getExitCode() == 0, "Check - return code");
+	}
+	
+	public void createProvider(){
+		provider_name = getText("provider.create.name")+" "+uid;
+		String provider_description = getText("provider.create.description");
+		KatelloProvider prov = new KatelloProvider(provider_name, org_name, provider_description, null);
+		SSHCommandResult res = prov.create();
+		Assert.assertTrue(res.getExitCode() == 0, "Check - return code");
+	}
+}
