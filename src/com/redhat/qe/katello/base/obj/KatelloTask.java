7a0269d9d37578c0910a8e61c5a677c1ddb16860
Garik Khachikyan <gkhachik@redhat.com>
2012-10-23 11:15:38 +0200

    cleanup unused stuff. Copy/paste.

--- src/com/redhat/qe/katello/base/obj/KatelloTask.java
+++ src/com/redhat/qe/katello/base/obj/KatelloTask.java
@@ -2,20 +2,10 @@ package com.redhat.qe.katello.base.obj;
 
 import java.util.logging.Logger;
 import org.codehaus.jackson.annotate.JsonProperty;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class KatelloTask extends _KatelloObject{
 	protected static Logger log = Logger.getLogger(KatelloTask.class.getName());
-	
-	// ** ** ** ** ** ** ** Public constants
-	public static final String LIBRARY = "Library";
-	
-	public static final String CMD_CREATE = "environment create";
-	public static final String CMD_INFO = "environment info -v";
-	public static final String CLI_CMD_LIST = "environment list -v";
-	public static final String CMD_DELETE = "environment delete";
-	public static final String CMD_UPDATE = "environment update";
 
 	@JsonProperty("task_type")
 	public String task_type;
