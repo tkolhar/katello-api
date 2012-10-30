56f6806eb36b103fc7e93915103239a910143952
Garik Khachikyan <gkhachik@redhat.com>
2012-10-30 12:52:42 +0100

    i18n Template cli tests. small fix in template.list() - contains -v option

--- src/com/redhat/qe/katello/base/obj/KatelloTemplate.java
+++ src/com/redhat/qe/katello/base/obj/KatelloTemplate.java
@@ -8,7 +8,7 @@ public class KatelloTemplate extends _KatelloObject{
 	// ** ** ** ** ** ** ** Public constants
 	public static final String CMD_CREATE = "template create";
 	public static final String CMD_DELETE = "template delete";
-	public static final String CMD_LIST = "template list";
+	public static final String CMD_LIST = "template list -v";
 	public static final String CMD_INFO = "template info -v";
 	public static final String CMD_UPDATE = "template update";
 	public static final String CMD_EXPORT = "template export";
