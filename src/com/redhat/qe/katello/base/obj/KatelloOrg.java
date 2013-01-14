a181b87f74ef38dce942d58b27e4172b7424e006
Garik Khachikyan <gkhachik@redhat.com>
2013-01-14 12:15:32 +0100

    org failflow - stderr changed:

--- src/com/redhat/qe/katello/base/obj/KatelloOrg.java
+++ src/com/redhat/qe/katello/base/obj/KatelloOrg.java
@@ -28,9 +28,9 @@ public class KatelloOrg extends _KatelloObject{
 	public static final String OUT_CREATE = 
 			"Successfully created org [ %s ]";
 	public static final String ERR_ORG_EXISTS_MUST_BE_UNIQUE = 
-			"Validation failed: Name has already been taken, Label has already been taken, Organization Names and labels must be unique across all organizations";
+			"Validation failed: Name has already been taken, Label already exists (including organizations being deleted), Organization Names and labels must be unique across all organizations";
 	public static final String ERR_ORG_EXISTS = 
-			"Validation failed: Name has already been taken, Label has already been taken";
+			"Validation failed: Name has already been taken, Label already exists (including organizations being deleted)";
 
 	public static final String ERR_NAME_INVALID = 
 			"Validation failed: Name cannot contain characters other than alpha numerals, space,'_', '-'.";
@@ -39,7 +39,7 @@ public class KatelloOrg extends _KatelloObject{
 	public static final String ERR_ORG_NAME_EXISTS = 
 			"Validation failed: Name has already been taken";
 	public static final String ERR_ORG_LABEL_EXISTS = 
-			"Validation failed: Label has already been taken";
+			"Validation failed: Label already exists (including organizations being deleted)";
 	
 	public static final String REG_ORG_LIST = ".*ID\\s*:\\s+\\d+.*Name\\s*:\\s+%s.*Description\\s*:\\s+%s.*";
 	public static final String REG_ORG_INFO = ".*ID\\s*:\\s+\\d+.*Name\\s*:\\s+%s.*Description\\s*:\\s+%s.*";
