952f1c22f4e89bb647ce8893234f1a38b8ffe5dc
tkolhar <tazimkolhar@gmail.com>
2013-01-24 13:41:37 +0530

    seperated katello-configure tests and modified it to run under deltacloud

---
+++ src/com/redhat/qe/katello/base/obj/DeltaCloudInstance.java
@@ -0,0 +1,59 @@
+package com.redhat.qe.katello.base.obj;
+
+import org.apache.deltacloud.client.DeltaCloudClientImpl;
+import org.apache.deltacloud.client.Instance;
+
+public class DeltaCloudInstance {
+
+	private String ipAddress;
+
+	private String hostName;
+
+	private String[] configs;
+
+	private Instance instance;
+
+	private DeltaCloudClientImpl client;
+
+	public String getIpAddress() {
+		return ipAddress;
+	}
+
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+
+	public Instance getInstance() {
+		return instance;
+	}
+
+	public void setInstance(Instance instance) {
+		this.instance = instance;
+	}
+
+	public DeltaCloudClientImpl getClient() {
+		return client;
+	}
+
+	public void setClient(DeltaCloudClientImpl client) {
+		this.client = client;
+	}
+
+	public String getHostName() {
+		return hostName;
+	}
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+
+	public String[] getConfigs() {
+		return configs;
+	}
+
+	public void setConfigs(String[] configs) {
+		this.configs = configs;
+	}
+
+}
+
--- src/com/redhat/qe/katello/common/KatelloConstants.java
+++ src/com/redhat/qe/katello/common/KatelloConstants.java
@@ -119,4 +119,6 @@ public interface KatelloConstants {
 	public static final String TNG_POST_UPGRADE = "post-upgrade";
 	
 	public static final String KATELLO_DEFAULT_LOCALE = "en_US";
+	public static final String[][] DELTACLOUD_CLIENTS = new String[][]{{"cfseclient1", "usersys.redhat.com", "335f800994d9adc5947b49728b14d527"}, {"cfseclient2", "usersys.redhat.com", "c9c0395f0730624541d8d26f7eb11be8"}, {"cfseclient3", "usersys.redhat.com", "edcf8c472ca601e33326f50f5d46ba8e"}};
+	public static final String[][] DELTACLOUD_SERVERS = new String[][]{{"cfseserver1", "usersys.redhat.com", "502f89b3921dcff92ccdfcf91a6e7db9", "10.16.120.72", "00:1a:4a:10:78:36"}};
 }
