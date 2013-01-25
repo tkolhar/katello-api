acaa3ed66a8daffa9b12b4325d82c9a17f8daf2b
Hayk Hovsepyan <hhovsepy@redhat.com>
2013-01-25 17:03:41 +0100

    some changes for CFSE

--- src/com/redhat/qe/katello/tests/installation/AnswerfileTests.java
+++ src/com/redhat/qe/katello/tests/installation/AnswerfileTests.java
@@ -22,19 +22,19 @@ public class AnswerfileTests extends KatelloCliTestScript{
 	private String deployment = KatelloConstants.KATELLO_PRODUCT;
 	private String engine = System.getProperty("katello.engine", "katello");
 
-	@Test(description = "katello-configure with answer file")
-	public void test_katello_configure_answer_file() {
-	
-		    
-			exec_result = KatelloUtils.sshOnServer("cp "+ answer_file +" "+ tmp_answer_file);
-			Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code Copying the contents to tmp answer file");
-			KatelloUtils.sshOnServer("sed -i -e \"s/org_name.*/org_name = "+ org_name +"/\" -e \"s/user_name.*/user_name = "+ user_name + "/\" -e \"s/\\buser_pass.*/user_pass = "+ user_pass + "/\" /tmp/answer-file");
-			exec_result = KatelloUtils.sshOnServer("katello-configure --deployment="+ deployment +" --answer-file="+ tmp_answer_file +" --reset-cache=YES --reset-data=YES");			
-			Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code Configure Successful");
-			exec_result = KatelloUtils.sshOnServer("katello-service status");
-			Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code Check all the Services Running");
-			exec_result = KatelloUtils.sshOnServer(engine +" -u "+ user_name + " -p "+ user_pass + " org list");
-			Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code Displayed the Org");
-	}  	  
+//	@Test(description = "katello-configure with answer file")
+//	public void test_katello_configure_answer_file() {
+//	
+//		    
+//			exec_result = KatelloUtils.sshOnServer("cp "+ answer_file +" "+ tmp_answer_file);
+//			Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code Copying the contents to tmp answer file");
+//			KatelloUtils.sshOnServer("sed -i -e \"s/org_name.*/org_name = "+ org_name +"/\" -e \"s/user_name.*/user_name = "+ user_name + "/\" -e \"s/\\buser_pass.*/user_pass = "+ user_pass + "/\" /tmp/answer-file");
+//			exec_result = KatelloUtils.sshOnServer("katello-configure --deployment="+ deployment +" --answer-file="+ tmp_answer_file +" --reset-cache=YES --reset-data=YES");			
+//			Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code Configure Successful");
+//			exec_result = KatelloUtils.sshOnServer("katello-service status");
+//			Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code Check all the Services Running");
+//			exec_result = KatelloUtils.sshOnServer(engine +" -u "+ user_name + " -p "+ user_pass + " org list");
+//			Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code Displayed the Org");
+//	}  	  
 }
 
