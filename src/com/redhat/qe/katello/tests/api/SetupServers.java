4c1214bccc40afd71a58489eebf17ddf4090a6d5
Hayk Hovsepyan <hhovsepy@redhat.com>
2013-02-12 10:41:26 +0100

    deltacloud changes merged

--- src/com/redhat/qe/katello/tests/api/SetupServers.java
+++ src/com/redhat/qe/katello/tests/api/SetupServers.java
@@ -8,30 +8,38 @@ import com.redhat.qe.katello.base.KatelloCliTestScript;
 import com.redhat.qe.katello.base.obj.DeltaCloudInstance;
 import com.redhat.qe.katello.common.KatelloUtils;
 
-@Test(groups = { "cfse-api" })
+@Test(groups = { "cfse-api", "headpin-api"})
 public class SetupServers extends KatelloCliTestScript {
 	
 	protected DeltaCloudInstance server;
 	protected DeltaCloudInstance client;
 	protected String server_name;
 	protected String client_name;
+	protected static boolean isDeltacloud = false;
 	
 	@BeforeSuite(description = "setup Deltacloud Server and client")
 	public void setUp() {
-		server = KatelloUtils.getDeltaCloudServer(1);
-		server_name = server.getHostName();
 		
-		client = KatelloUtils.getDeltaCloudClient(server_name, 1);
-		client_name = client.getHostName();
+		isDeltacloud = Boolean.parseBoolean(System.getProperty("runondeltacloud", "false"));
 		
-		System.setProperty("katello.server.hostname", server.getHostName());
-		System.setProperty("katello.client.hostname", client.getHostName());
+		if (isDeltacloud) {
+			server = KatelloUtils.getDeltaCloudServer(1);
+			server_name = server.getHostName();
+			
+			client = KatelloUtils.getDeltaCloudClient(server_name, 1);
+			client_name = client.getHostName();
+			
+			System.setProperty("katello.server.hostname", server_name);
+			System.setProperty("katello.client.hostname", client_name);
+		}
 	}
 	
 	@AfterSuite
 	public void tearDown() {
-		KatelloUtils.destroyDeltaCloudMachine(server);
-		KatelloUtils.destroyDeltaCloudMachine(client);
+		if (isDeltacloud) {
+			KatelloUtils.destroyDeltaCloudMachine(server);
+			KatelloUtils.destroyDeltaCloudMachine(client);
+		}
 	}
 
 }
