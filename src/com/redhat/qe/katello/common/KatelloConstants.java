b29ebae56617df682399501e0f2fe3330e458342
Garik Khachikyan <gkhachik@redhat.com>
2013-05-10 16:34:05 +0200

    more fixes on e2e:

--- src/com/redhat/qe/katello/common/KatelloConstants.java
+++ src/com/redhat/qe/katello/common/KatelloConstants.java
@@ -29,6 +29,7 @@ public interface KatelloConstants {
 
 	// Some repo URLs
 	public static final String REPO_INECAS_ZOO3 = "http://inecas.fedorapeople.org/fakerepos/zoo3/";
+	public static final String REPO_HHOVSEPY_ZOO4 = "http://hhovsepy.fedorapeople.org/fakerepos/zoo4";
 
 	/** curl -sk -u {username}:{password} 
 	 * https://${servername}/api${call} */
