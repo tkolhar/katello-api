9f274ce854c788ff2976b246bf3cef37be197288
tkolhar <tkolhar@gmail.com>
2013-06-06 15:22:06 +0530

    Added test ID 274318

--- src/com/redhat/qe/katello/base/obj/KatelloDistributor.java
+++ src/com/redhat/qe/katello/base/obj/KatelloDistributor.java
@@ -9,7 +9,11 @@ public class KatelloDistributor extends _KatelloObject{
 	public static final String CMD_CREATE = "distributor create";
 	public static final String CMD_INFO = "distributor info";
 	public static final String CMD_ADD_CUSTOM_INFO = "distributor add_custom_info";
+	public static final String CMD_REMOVE_CUSTOM_INFO = "distributor remove_custom_info";
 	public static final String OUT_CREATE = "Successfully createed distributor [ %s ]";
+	public static final String OUT_INFO = "Successfully added Custom Information [ %s : %s ] to Distributor [ %s ]";
+	public static final String OUT_REMOVE_INFO = "Could not remove Custom Information from Distributor [ %s ]";
+	public static final String OUT_REMOVE_INVALID_KEY = "Couldn't find custom info with keyname '%s'";
 
 	// ** ** ** ** ** ** ** Class members
 	String org_name;
@@ -34,13 +38,24 @@ public class KatelloDistributor extends _KatelloObject{
 		return run(CMD_INFO);
 	}
 	
-	public SSHCommandResult add_info(String keyname,String value){
+	public SSHCommandResult add_info(String keyname,String value,String uuid){
+		opts.clear();
+		opts.add(new Attribute("org",this.org_name));
+		if(uuid == null){
+			opts.add(new Attribute("name",this.dis_name));
+		}
+		opts.add(new Attribute("uuid",uuid));
+		opts.add(new Attribute("keyname",keyname));
+		opts.add(new Attribute("value",value));
+		return run(CMD_ADD_CUSTOM_INFO);		
+	}
+	
+	public SSHCommandResult remove_info(String keyname){
 		opts.clear();
 		opts.add(new Attribute("org",this.org_name));
 		opts.add(new Attribute("name",this.dis_name));
 		opts.add(new Attribute("keyname",keyname));
-		opts.add(new Attribute("value",value));
-		return run(CMD_ADD_CUSTOM_INFO);		
+		return run(CMD_REMOVE_CUSTOM_INFO);		
 	}
 }
 
--- src/com/redhat/qe/katello/tests/cli/KatelloDistributorTests.java
+++ src/com/redhat/qe/katello/tests/cli/KatelloDistributorTests.java
@@ -3,6 +3,7 @@ package com.redhat.qe.katello.tests.cli;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import com.redhat.qe.Assert;
+import com.redhat.qe.katello.base.KatelloCli;
 import com.redhat.qe.katello.base.KatelloCliDataProvider;
 import com.redhat.qe.katello.base.KatelloCliTestScript;
 import com.redhat.qe.katello.base.obj.KatelloDistributor;
@@ -32,8 +33,44 @@ public class KatelloDistributorTests extends KatelloCliTestScript{
 			dataProvider="add_distributor_custom_info", dataProviderClass = KatelloCliDataProvider.class, enabled=true,groups={"cfse-cli","headpin-cli"})
 	public void test_distributorAddCustomInfo(String keyname, String value, Integer exitCode){
 		SSHCommandResult exec_result;
-		exec_result = distributor.add_info(keyname, value);
+		exec_result = distributor.add_info(keyname, value, null);
 		Assert.assertTrue(exec_result.getExitCode().intValue() == exitCode.intValue(), "Check - return code");		
 	} 
+	
+	@Test(description="distributor remove custom info",enabled=true,groups={"cfse-cli","headpin-cli"})
+	public void test_distributorRemoveCustomInfo(){
+		SSHCommandResult exec_result;
+		uid = KatelloUtils.getUniqueID();
+		String org_rm_name="org_rm-" + uid;
+		String dis_rm_name="dis_rm-" + uid;
+		String test_key = "test_key" + uid;
+		String test_val = "test_val" + uid;
+		String invalid_key = "invalid_key" + uid;
+		KatelloOrg org_rm=new KatelloOrg(org_rm_name,"Org for remove distributor");
+		exec_result = org_rm.cli_create();
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+		Assert.assertEquals(getOutput(exec_result).trim(),String.format(KatelloOrg.OUT_CREATE,org_rm_name));
+		KatelloDistributor dis_rm = new KatelloDistributor(org_rm_name,dis_rm_name);
+		exec_result = dis_rm.distributor_create();
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+		Assert.assertEquals(getOutput(exec_result).trim(),String.format(KatelloDistributor.OUT_CREATE,dis_rm_name));
+		exec_result = dis_rm.add_info(test_key, test_val,null);
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+		Assert.assertEquals(getOutput(exec_result).trim(),String.format(KatelloDistributor.OUT_INFO,test_key,test_val,dis_rm_name));
+		exec_result = dis_rm.distributor_info();
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+		String dis_uuid = KatelloCli.grepCLIOutput("UUID", getOutput(exec_result));
+		exec_result = dis_rm.remove_info(test_key);
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+		Assert.assertEquals(getOutput(exec_result).trim(),String.format(KatelloDistributor.OUT_REMOVE_INFO,dis_rm_name));
+		exec_result = dis_rm.distributor_info();
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+		exec_result = dis_rm.remove_info(invalid_key);
+		Assert.assertTrue(exec_result.getExitCode() == 148, "Check - return code");
+		Assert.assertEquals(getOutput(exec_result).trim(),String.format(KatelloDistributor.OUT_REMOVE_INVALID_KEY,invalid_key));
+		exec_result = dis_rm.add_info(test_key,test_val,dis_uuid);
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+		Assert.assertEquals(getOutput(exec_result).trim(),String.format(KatelloDistributor.OUT_INFO,test_key,test_val,dis_uuid));				
+	}
 }
 
