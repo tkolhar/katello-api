b39fac468a72c8c037416e8d87cdb28ae53791ea
tkolhar <tkolhar@gmail.com>
2013-06-10 14:33:22 +0530

    Fixed the test ID 274318

--- src/com/redhat/qe/katello/base/obj/KatelloDistributor.java
+++ src/com/redhat/qe/katello/base/obj/KatelloDistributor.java
@@ -10,7 +10,7 @@ public class KatelloDistributor extends _KatelloObject{
 	public static final String CMD_INFO = "distributor info";
 	public static final String CMD_ADD_CUSTOM_INFO = "distributor add_custom_info";
 	public static final String CMD_REMOVE_CUSTOM_INFO = "distributor remove_custom_info";
-	public static final String OUT_CREATE = "Successfully createed distributor [ %s ]";
+	public static final String OUT_CREATE = "Successfully created distributor [ %s ]";
 	public static final String OUT_INFO = "Successfully added Custom Information [ %s : %s ] to Distributor [ %s ]";
 	public static final String OUT_REMOVE_INFO = "Could not remove Custom Information from Distributor [ %s ]";
 	public static final String OUT_REMOVE_INVALID_KEY = "Couldn't find custom info with keyname '%s'";
@@ -18,6 +18,7 @@ public class KatelloDistributor extends _KatelloObject{
 	// ** ** ** ** ** ** ** Class members
 	String org_name;
 	String dis_name;
+	String uuid;
 
 	public KatelloDistributor(String dOrg,String dName){
 		this.org_name = dOrg;
@@ -38,12 +39,10 @@ public class KatelloDistributor extends _KatelloObject{
 		return run(CMD_INFO);
 	}
 	
-	public SSHCommandResult add_info(String keyname,String value,String uuid){
+	public SSHCommandResult add_info(String keyname,String value,String uuid,String dis_name,String org_name){
 		opts.clear();
-		opts.add(new Attribute("org",this.org_name));
-		if(uuid == null){
-			opts.add(new Attribute("name",this.dis_name));
-		}
+		opts.add(new Attribute("org",org_name));
+		opts.add(new Attribute("name",dis_name));
 		opts.add(new Attribute("uuid",uuid));
 		opts.add(new Attribute("keyname",keyname));
 		opts.add(new Attribute("value",value));
