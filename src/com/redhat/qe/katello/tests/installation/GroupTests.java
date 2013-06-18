c783f7aab429f8bc8434fc338c65e4b4dd30ddb5
Hayk Hovsepyan <hhovsepy@redhat.com>
2013-06-18 10:27:46 +0200

    fixing failing tests

--- src/com/redhat/qe/katello/tests/installation/GroupTests.java
+++ src/com/redhat/qe/katello/tests/installation/GroupTests.java
@@ -13,7 +13,7 @@ import com.redhat.qe.tools.SSHCommandResult;
 public class GroupTests extends KatelloCliTestScript {
 	
 	private String role_name;
-	private final String LDAP_GROUP = "admins";
+	private final String LDAP_GROUP = "admin-group";
 	private final String INVALID_LDAP_GROUP = "exgirlfriends";
 	
 	@BeforeClass(description="init: create role stuff")
@@ -44,8 +44,8 @@ public class GroupTests extends KatelloCliTestScript {
 	public void test_addInvalidGroup() {
 		KatelloUserRole role = new KatelloUserRole(role_name, "test role");
 		SSHCommandResult res = role.cli_add_ldap_group(INVALID_LDAP_GROUP);
-		Assert.assertTrue(res.getExitCode().intValue() == 144,"Check - error code (user role add ldap group)");
-		Assert.assertTrue(getOutput(res).contains("error"), "Check error code");
+		Assert.assertTrue(res.getExitCode().intValue() == 166,"Check - error code (user role add ldap group)");
+		Assert.assertTrue(getOutput(res).contains("Validation failed: Ldap group does not exist in your current LDAP system. Please choose a different group, or contact your LDAP administrator to have this group created"), "Check error code");
 		res = role.cli_info();
 		Assert.assertTrue(res.getExitCode().intValue() == 0,"Check - return code (user role info)");
 		Assert.assertFalse(getOutput(res).contains(LDAP_GROUP), LDAP_GROUP + " is not in role.");
