50d7d429d2e5de889cf30dc90b852d85a953c1e0
Garik Khachikyan <gkhachik@redhat.com>
2013-06-21 15:57:41 +0200

    just comments.

--- src/com/redhat/qe/katello/common/TngRunGroups.java
+++ src/com/redhat/qe/katello/common/TngRunGroups.java
@@ -16,11 +16,11 @@ public interface TngRunGroups {
 	public static final String TNG_KATELLO_Subscriptions = "Subscriptions";
 	public static final String TNG_KATELLO_Content = "Content"; // tcms--
 	public static final String TNG_KATELLO_Providers_Repos = "Providers / Repos"; // tcms--
-	public static final String TNG_KATELLO_System_Groups = "System Groups";
-	public static final String TNG_KATELLO_System_Consumer = "System / Consumer";
-	public static final String TNG_KATELLO_Organizations = "Organizations";
-	public static final String TNG_KATELLO_Manifests_CDN = "Manifests / CDN";
-	public static final String TNG_KATELLO_Users_Roles = "Users / Roles";
+	public static final String TNG_KATELLO_System_Groups = "System Groups"; // tcms++
+	public static final String TNG_KATELLO_System_Consumer = "System / Consumer"; // tcms++
+	public static final String TNG_KATELLO_Organizations = "Organizations"; // tcms++
+	public static final String TNG_KATELLO_Manifests_CDN = "Manifests / CDN"; // tcms++
+	public static final String TNG_KATELLO_Users_Roles = "Users / Roles"; // tcms++
 	public static final String TNG_KATELLO_Install_Configuration = "Install / Configuration";
 	public static final String TNG_KATELLO_DEFAULT = "--default--";
 }
