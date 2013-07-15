9a661d3edaac4a5acfbde7e113a97034a0be827d
Hayk Hovsepyan <hhovsepy@redhat.com>
2013-07-15 12:17:27 +0200

    refactored to reuse content, fixed failing tests

--- src/com/redhat/qe/katello/tests/e2e/ConsumeFilteredErrata.java
+++ src/com/redhat/qe/katello/tests/e2e/ConsumeFilteredErrata.java
@@ -1,6 +1,5 @@
 package com.redhat.qe.katello.tests.e2e;
 
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -10,28 +9,18 @@ import com.redhat.qe.katello.base.obj.KatelloActivationKey;
 import com.redhat.qe.katello.base.obj.KatelloContentDefinition;
 import com.redhat.qe.katello.base.obj.KatelloContentFilter;
 import com.redhat.qe.katello.base.obj.KatelloContentView;
-import com.redhat.qe.katello.base.obj.KatelloEnvironment;
 import com.redhat.qe.katello.base.obj.KatelloOrg;
-import com.redhat.qe.katello.base.obj.KatelloProduct;
-import com.redhat.qe.katello.base.obj.KatelloProvider;
-import com.redhat.qe.katello.base.obj.KatelloRepo;
 import com.redhat.qe.katello.base.obj.KatelloSystem;
 import com.redhat.qe.katello.base.obj.KatelloSystemGroup;
 import com.redhat.qe.katello.base.obj.helpers.FilterRuleErrataDayType;
 import com.redhat.qe.katello.base.obj.helpers.FilterRuleErrataIds;
 import com.redhat.qe.katello.common.KatelloUtils;
 import com.redhat.qe.katello.common.TngRunGroups;
-import com.redhat.qe.tools.SSHCommandResult;
 
 @Test(groups=TngRunGroups.TNG_KATELLO_Content, singleThreaded = true)
 public class ConsumeFilteredErrata extends KatelloCliTestBase {
 	
 	String uid = KatelloUtils.getUniqueID();
-	String org_name = "orgcon-"+ uid;
-	String env_name = "envcon-"+ uid;
-	String prov_name = "provcon-" + uid;
-	String prod_name = "prodcon-"+ uid;
-	String repo_name = "repocon-" + uid;
 	String condef_name = "condef-" + uid;
 	String errata_filter = "errata filter";
 	String pubview_name = "pubview-" + uid;
@@ -39,13 +28,7 @@ public class ConsumeFilteredErrata extends KatelloCliTestBase {
 	String act_key_name = "act_key-" + uid;
 	String group_name = "group" + uid;
 	String system_uuid1;
-	
-	SSHCommandResult exec_result;
-	KatelloOrg org;
-	KatelloEnvironment env;
-	KatelloProvider prov;
-	KatelloProduct prod;
-	KatelloRepo repo;
+
 	KatelloContentDefinition condef;
 	KatelloContentView conview;
 	KatelloActivationKey act_key;
@@ -72,41 +55,18 @@ public class ConsumeFilteredErrata extends KatelloCliTestBase {
 	public void setUp() {
 		sshOnClient("yum erase -y kangaroo sheep zebra bird rat ferret crab rabbit eagle fish monkey polecat fox seal bat --disablerepo \\* || true");
 		
-		org = new KatelloOrg(this.cli_worker, org_name,null);
-		exec_result = org.cli_create();		              
-		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		
-		env = new KatelloEnvironment(this.cli_worker, env_name,null,org_name,KatelloEnvironment.LIBRARY);
-		exec_result = env.cli_create();
-		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		
-		prov = new KatelloProvider(this.cli_worker, prov_name,org_name,null,null);
-		exec_result = prov.create();
-		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		
-		prod = new KatelloProduct(this.cli_worker, prod_name,org_name,prov_name,null, null, null,null, null);
-		exec_result = prod.create();
-		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		
-		repo = new KatelloRepo(this.cli_worker, repo_name,org_name,prod_name, REPO_HHOVSEPY_ZOO4, null, null);
-		exec_result = repo.create(true);
-		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-
-		exec_result = repo.synchronize();
-		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		
-		condef = new KatelloContentDefinition(cli_worker, condef_name,null,org_name,null);
+		condef = new KatelloContentDefinition(cli_worker, condef_name,null,base_org_name,null);
 		exec_result = condef.create();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 
-		exec_result = condef.add_product(prod_name);
+		exec_result = condef.add_product(base_zoo4_product_name);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
-		exec_result = condef.add_repo(prod_name, repo_name);
+		exec_result = condef.add_repo(base_zoo4_product_name, base_zoo4_repo_name);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
 		// The only way to install ERRATA on System is by system group
-		group = new KatelloSystemGroup(this.cli_worker, group_name, this.org_name);
+		group = new KatelloSystemGroup(this.cli_worker, group_name, base_org_name);
 		exec_result = group.create();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
@@ -119,16 +79,16 @@ public class ConsumeFilteredErrata extends KatelloCliTestBase {
 	@Test(description="Consume content from filtered errata")
 	public void test_consumeErrataContent() {
 
-		KatelloContentFilter filter = new KatelloContentFilter(cli_worker, errata_filter, org_name, condef_name);
+		KatelloContentFilter filter = new KatelloContentFilter(cli_worker, errata_filter, base_org_name, condef_name);
 
 		exec_result = filter.create();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 
 		exec_result = filter.info();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		exec_result = filter.add_repo(prod_name, repo_name);
+		exec_result = filter.add_repo(base_zoo4_product_name, base_zoo4_repo_name);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		Assert.assertTrue(getOutput(exec_result).equals(String.format(KatelloContentFilter.OUT_ADD_REPO, repo_name, errata_filter)), "Check output");
+		Assert.assertTrue(getOutput(exec_result).equals(String.format(KatelloContentFilter.OUT_ADD_REPO, base_zoo4_repo_name, errata_filter)), "Check output");
 		
 		
 		exec_result = filter.add_rule(KatelloContentFilter.TYPE_INCLUDES, new FilterRuleErrataIds(KANGAROO_ERRATA, FISH_ERRATA));
@@ -155,18 +115,18 @@ public class ConsumeFilteredErrata extends KatelloCliTestBase {
 		
 		condef.publish(pubview_name,pubview_name,null);
 
-		conview = new KatelloContentView(cli_worker, pubview_name, org_name);
-		exec_result = conview.promote_view(env_name);
+		conview = new KatelloContentView(cli_worker, pubview_name, base_org_name);
+		exec_result = conview.promote_view(base_test_env_name);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		Assert.assertTrue(getOutput(exec_result).contains(String.format(KatelloContentView.OUT_PROMOTE, this.pubview_name, env_name)), "Content view promote output.");
+		Assert.assertTrue(getOutput(exec_result).contains(String.format(KatelloContentView.OUT_PROMOTE, this.pubview_name, base_test_env_name)), "Content view promote output.");
 		// promoting content view is the same as create changeset, add content view and promote, it is completely legal
 		
-		act_key = new KatelloActivationKey(this.cli_worker, org_name,env_name,act_key_name,"Act key created");
+		act_key = new KatelloActivationKey(this.cli_worker, base_org_name, base_test_env_name, act_key_name,"Act key created");
 		exec_result = act_key.create();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");      
 		exec_result = act_key.update_add_content_view(pubview_name);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");      
-		exec_result = new KatelloOrg(this.cli_worker, this.org_name,null).subscriptions();
+		exec_result = new KatelloOrg(this.cli_worker, base_org_name,null).subscriptions();
 		String zoo4PoolId = KatelloUtils.grepCLIOutput("ID", getOutput(exec_result), 1);
 		exec_result = act_key.update_add_subscription(zoo4PoolId);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
@@ -178,11 +138,11 @@ public class ConsumeFilteredErrata extends KatelloCliTestBase {
 
 		//register client, subscribe to pool
 		rhsm_clean();
-		sys = new KatelloSystem(this.cli_worker, system_name1, this.org_name, null);
+		sys = new KatelloSystem(this.cli_worker, system_name1, base_org_name, null);
 		exec_result = sys.rhsm_registerForce(act_key_name);
 		Assert.assertTrue(exec_result.getExitCode().intValue() == 0, "Check - return code");
 		
-		sys.setEnvironmentName(this.env_name);
+		sys.setEnvironmentName(base_test_env_name);
 		exec_result = sys.list();
 		Assert.assertTrue(exec_result.getExitCode().intValue() == 0, "Check - return code");
 		system_uuid1 = KatelloUtils.grepCLIOutput("UUID", getOutput(exec_result));
@@ -190,7 +150,8 @@ public class ConsumeFilteredErrata extends KatelloCliTestBase {
 		exec_result = group.add_systems(system_uuid1);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
-//		yum_clean();			
+		KatelloUtils.sshOnClient(cli_worker.getClientHostname(), "subscription-manager refresh; service rhsmcertd restart");
+		yum_clean();			
 		
 		// consume older packages of erratas
 		install_Packages(cli_worker.getClientHostname(),new String[] {"kangaroo-3.6.6-1", "sheep-1.3.6-1", "zebra-10.0.7-1", "bird-5.1.10-1", "rat-7.7.0-1", "ferret-1.2.0-1", "crab-5.5.1-1", 
