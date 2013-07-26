753047ee86cfea4ae8168ea74caa2e879584b84b
Hayk Hovsepyan <hhovsepy@redhat.com>
2013-07-26 12:06:33 +0200

    added support on rhel5 client and repo

--- src/com/redhat/qe/katello/tests/installation/TestMultipleAgents.java
+++ src/com/redhat/qe/katello/tests/installation/TestMultipleAgents.java
@@ -34,8 +34,17 @@ public class TestMultipleAgents extends KatelloCliTestBase {
 	protected String zoo_product_id = null;
 	protected String zoo_repo_name = null;
 	protected String zoo_repo_pool = null;
+	protected String rhel5_zoo_provider_name = null;
+	protected String rhel5_zoo_product_name = null;
+	protected String rhel5_zoo_product_id = null;
+	protected String rhel5_zoo_repo_name = null;
+	protected String rhel5_zoo_repo_pool = null;
+
 	protected String act_key_name = null;
 	protected String package_name = "lion";
+
+	protected String rhel5_act_key_name = null;
+	protected String rhel5_package_name = "lion";
 	
 	@Test(description = "setup Deltacloud Server")
 	public void testMultipleClients() {
@@ -45,11 +54,6 @@ public class TestMultipleAgents extends KatelloCliTestBase {
 		System.setProperty("katello.server.hostname", server_name);
 		System.setProperty("katello.client.hostname", server_name);
 
-		try {
-			Thread.sleep(600000);
-		} catch (InterruptedException iex) {
-		}
-
 		createOrgStuff();
 		
 		StringTokenizer tok = new StringTokenizer(
@@ -57,14 +61,9 @@ public class TestMultipleAgents extends KatelloCliTestBase {
 		
 		while (tok.hasMoreTokens()) {
 			String type = tok.nextToken().trim();
-			DeltaCloudInstance client = KatelloUtils.getDeltaCloudClient(
+			DeltaCloudInstance client = KatelloUtils.getDeltaCloudClientCertOnly(
 					server_name, DELTACLOUD_IMAGES.get(type));
 			clients.add(client);
-			
-			try {
-				Thread.sleep(300000);
-			} catch (InterruptedException iex) {
-			}
 			
 			testClientConsume(client.getHostName(), type);
 			
@@ -88,6 +87,11 @@ public class TestMultipleAgents extends KatelloCliTestBase {
 		zoo_product_name = "Zoo Prod " + uid;
 		zoo_repo_name = "Zoo Repo " + uid;
 		act_key_name = "activationkey" + uid;
+
+		rhel5_zoo_provider_name = "RHEL5 Zoo Prov " + uid;
+		rhel5_zoo_product_name = "RHEL5 Zoo Prod " + uid;
+		rhel5_zoo_repo_name = "RHEL5 Zoo Repo " + uid;
+		rhel5_act_key_name = "rhel5activationkey" + uid;
 
 		KatelloOrg org = new KatelloOrg(null, org_name, null);
 		exec_result = org.cli_create();
@@ -116,26 +120,63 @@ public class TestMultipleAgents extends KatelloCliTestBase {
 		Assert.assertNotNull(zoo_product_id, "Check - zoo_product_id is not null");
 		
 		String view = KatelloUtils.promoteProductToEnvironment(null, org_name, zoo_product_name, dev_env_name);
-		
 		KatelloActivationKey act_key = new KatelloActivationKey(null, org_name, dev_env_name, act_key_name,"Act key created", null, view);
 		exec_result = act_key.create();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");           
-
 		exec_result = act_key.update_add_subscription(zoo_repo_pool);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+
+		prov = new KatelloProvider(null, rhel5_zoo_provider_name, org_name, "rhel5 Zoo provider", null);
+		exec_result = prov.create();
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+		// -- Product
+		prod = new KatelloProduct(null, rhel5_zoo_product_name, org_name, rhel5_zoo_provider_name, null, null, null, null, null);
+		exec_result = prod.create();
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+		// -- Repo
+		repo = new KatelloRepo(null, rhel5_zoo_repo_name, org_name, rhel5_zoo_product_name, "http://inecas.fedorapeople.org/fakerepos/zoo-rhel5-new/", null, null);
+		exec_result = repo.create(true);
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+		exec_result = repo.synchronize();
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+		rhel5_zoo_repo_pool = org.custom_getPoolId(rhel5_zoo_product_name);
+		Assert.assertNotNull(rhel5_zoo_repo_pool, "Check - pool Id is not null");
+		rhel5_zoo_product_id = prod.custom_getProductId();
+		Assert.assertNotNull(rhel5_zoo_product_id, "Check - zoo_product_id is not null");
+		
+		view = KatelloUtils.promoteProductToEnvironment(null, org_name, rhel5_zoo_product_name, dev_env_name);
+		act_key = new KatelloActivationKey(null, org_name, dev_env_name, rhel5_act_key_name,"Act key created", null, view);
+		exec_result = act_key.create();
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");           
+		exec_result = act_key.update_add_subscription(rhel5_zoo_repo_pool);
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+
 	}
 	
 	private void testClientConsume(String client_name, String client_type) {
+		String actKey = null;
+		String packName = null;
+		
+		if (client_type.matches(".*RHEL\\s+5.*")) {
+			actKey = rhel5_act_key_name;
+			packName = rhel5_package_name;
+		} else {
+			actKey = act_key_name;
+			packName = package_name;
+		}
+		
 		KatelloSystem sys = new KatelloSystem(null, client_name+KatelloUtils.getUniqueID(), org_name, null);
 		sys.runOn(client_name);
-		exec_result = sys.rhsm_registerForce(act_key_name);
+		exec_result = sys.rhsm_registerForce(actKey);
 		Assert.assertTrue(exec_result.getExitCode().intValue() == 0, "Check - return code");
 		
-		SSHCommandResult res = KatelloUtils.sshOnClient(client_name, "yum install -y " + package_name);
+		yum_clean(client_name);
+		
+		SSHCommandResult res = KatelloUtils.sshOnClient(client_name, "yum install -y " + packName);
 		Assert.assertTrue(res.getExitCode() == 0, "Package is installed successfully on machine " + client_type);
 		
-		res = KatelloUtils.sshOnClient(client_name, "rpm -qa | grep -E \"" + package_name + "\"");
-		Assert.assertTrue(getOutput(res).contains(package_name), "Package " + package_name + " should be installed on machine " + client_type);		
+		res = KatelloUtils.sshOnClient(client_name, "rpm -qa | grep -E \"" + packName + "\"");
+		Assert.assertTrue(getOutput(res).contains(packName), "Package " + packName + " should be installed on machine " + client_type);		
 	}
 
 }
