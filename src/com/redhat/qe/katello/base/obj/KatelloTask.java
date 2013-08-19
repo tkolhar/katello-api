dd9de927bd18bfaa85500b1431020a570706e149
Ales Dujicek <adujicek@redhat.com>
2013-08-19 10:56:37 +0200

    coverage: task command

--- src/com/redhat/qe/katello/base/obj/KatelloTask.java
+++ src/com/redhat/qe/katello/base/obj/KatelloTask.java
@@ -1,8 +1,11 @@
 package com.redhat.qe.katello.base.obj;
 
 import java.util.logging.Logger;
+import javax.management.Attribute;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.json.simple.JSONObject;
+import com.redhat.qe.katello.base.threading.KatelloCliWorker;
+import com.redhat.qe.tools.SSHCommandResult;
 
 public class KatelloTask extends _KatelloObject{
 	protected static Logger log = Logger.getLogger(KatelloTask.class.getName());
@@ -54,7 +57,42 @@ public class KatelloTask extends _KatelloObject{
 	
 	@JsonProperty("id")
 	public Long id;
+
+	public String org;
 	
+	public static final String CMD_STATUS = "task status";
+	public static final String CMD_LIST = "task list -v";
+
+	public static final String ERR_NOT_FOUND = "Could not find task [ %s ].";
+	public static final String ERR_INVALID_STATE = "State '%s' not valid. It must be in [waiting, running, error, finished, canceled, timed_out].";
+
+	public static final String REG_STATUS = "^.*Task Status.*UUID +: [0-9a-f-]+.*State +: .*Type +: .*Progress +: .*Start Time +: [0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}.*Finish Time +: [0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}.*Owner ID +: .*Owner Type +: .*$";
+
 	public KatelloTask(){super();}
-	
+
+	public KatelloTask(KatelloCliWorker kcr, String org, String uuid) {
+		this.kcr = kcr;
+		this.org = org;
+		this.uuid = uuid;
+	}
+
+	public SSHCommandResult status() {
+		opts.clear();
+		opts.add(new Attribute("uuid", uuid));
+		return run(CMD_STATUS);
+	}
+
+	public SSHCommandResult list() {
+		opts.clear();
+		opts.add(new Attribute("org", org));
+		return run(CMD_LIST);
+	}
+
+	public SSHCommandResult list(String state, String type) {
+		opts.clear();
+		opts.add(new Attribute("org", org));
+		opts.add(new Attribute("state", state));
+		opts.add(new Attribute("type", type));
+		return run(CMD_LIST);
+	}
 }
---
+++ src/com/redhat/qe/katello/tests/cli/TaskTests.java
@@ -0,0 +1,63 @@
+package com.redhat.qe.katello.tests.cli;
+
+import com.redhat.qe.Assert;
+import org.testng.annotations.Test;
+
+import com.redhat.qe.katello.base.KatelloCliTestBase;
+import com.redhat.qe.katello.base.obj.KatelloTask;
+import com.redhat.qe.katello.common.KatelloUtils;
+import com.redhat.qe.tools.SSHCommandResult;
+
+public class TaskTests extends KatelloCliTestBase {
+
+	public SSHCommandResult exec_result;
+	
+	@Test(description="list all tasks")
+	public void test_listAll() {
+		KatelloTask task = new KatelloTask(cli_worker, base_org_name, null);
+		exec_result = task.list();
+		Assert.assertTrue(exec_result.getExitCode()==0, "Check exit code (list all tasks)");
+	}
+
+	@Test(description="list tasks by status")
+	public void test_listByStatus() {
+		KatelloTask task = new KatelloTask(cli_worker, base_org_name, null);
+		exec_result = task.list("finished", null);
+		Assert.assertTrue(exec_result.getExitCode()==0, "Check exit code (list tasks by state)");
+	}
+
+	@Test(description="list tasks by type")
+	public void test_listByType() {
+		KatelloTask task = new KatelloTask(cli_worker, base_org_name, null);
+		exec_result = task.list(null, "TaskStatus");
+		Assert.assertTrue(exec_result.getExitCode()==0, "Check exit code (list tasks by type)");
+	}
+
+	@Test(description="task status")
+	public void test_taskState() {
+		KatelloTask task = new KatelloTask(cli_worker, base_org_name, null);
+		exec_result = task.list();
+		Assert.assertTrue(exec_result.getExitCode()==0, "Check exit code (list tasks)");
+		task.uuid = KatelloUtils.grepCLIOutput("UUID", getOutput(exec_result));
+		exec_result = task.status();
+		Assert.assertTrue(exec_result.getExitCode()==0, "Check exit code (task status)");
+		Assert.assertTrue(getOutput(exec_result).replace("\n", "").matches(KatelloTask.REG_STATUS), "Check output (task status)");
+	}
+
+	@Test(description="task status - wrong id")
+	public void test_taskStatusWrongID() {
+		KatelloTask task = new KatelloTask(cli_worker, base_org_name, KatelloUtils.getUniqueID());
+		exec_result = task.status();
+		Assert.assertTrue(exec_result.getExitCode()==65, "Check exit code (task status)");
+		Assert.assertTrue(getOutput(exec_result).equals(String.format(KatelloTask.ERR_NOT_FOUND, task.uuid)), "Check output (task status)");
+	}
+
+	@Test(description="list tasks by status - wrong status")
+	public void test_listByWrongStatus() {
+		String state = "wrong-state";
+		KatelloTask task = new KatelloTask(cli_worker, base_org_name, null);
+		exec_result = task.list(state, null);
+		Assert.assertTrue(exec_result.getExitCode()==65, "Check exit code (list tasks by state)");
+		Assert.assertTrue(getOutput(exec_result).equals(String.format(KatelloTask.ERR_INVALID_STATE, state)), "Check output (task list)");
+	}
+}
