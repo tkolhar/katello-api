b99046039cef30c10a39e4a66780a6e0aba680f9
Garik Khachikyan <gkhachik@redhat.com>
2013-10-29 16:40:42 +0100

    a bit refactoring, small logic change too.

--- src/com/redhat/qe/katello/tests/cli/CompositeContentViewTests.java
+++ src/com/redhat/qe/katello/tests/cli/CompositeContentViewTests.java
@@ -16,27 +16,27 @@ public class CompositeContentViewTests extends KatelloCliTestBase{
 	
 	String uid = KatelloUtils.getUniqueID();
 	String del_changeset_name = "del_changeset-" + uid;
-	String condef_name1 = "condef1-" + uid;
-	String condef_name2 = "condef2-" + uid;
-	String condef_name3 = "condef3-" + uid;
-	String condef_composite_name = "condefcomposite-" + uid;
-	String condef_composite_name2 = "condefcomposite2-" + uid;
-	String pubview_name1_1 = "pubview1-1" + uid;
-	String pubview_name1_2 = "pubview1-2" + uid;
-	String pubview_name2_1 = "pubview2-1" + uid;
-	String pubview_name2_2 = "pubview2-2" + uid;
-	String pubview_name3_1 = "pubview3-1" + uid;
-	String pubview_name3_2 = "pubview3-2" + uid;
+	String contDefName1 = "condef1-" + uid;
+	String contDefName2 = "condef2-" + uid;
+	String contDefName3 = "condef3-" + uid;
+	String cdCompositeName = "condefcomposite-" + uid;
+	String cdCompositeName2 = "condefcomposite2-" + uid;
+	String contViewName1_1 = "pubview1-1" + uid;
+	String contViewName1_2 = "pubview1-2" + uid;
+	String contViewName2_1 = "pubview2-1" + uid;
+	String contViewName2_2 = "pubview2-2" + uid;
+	String contViewName3_1 = "pubview3-1" + uid;
+	String contViewName3_2 = "pubview3-2" + uid;
 	String pubcompview_name1 = "pubcompview1" + uid;
 	String pubcompview_name2 = "pubcompview2" + uid;
 	String act_key_name2 = "act_key2" + uid;
 	String system_name2 = "system2" + uid;
 
 	KatelloChangeset del_changeset;
-	KatelloContentDefinition condef1;
-	KatelloContentDefinition condef2;
-	KatelloContentDefinition condef3;
-	KatelloContentDefinition compcondef;
+	KatelloContentDefinition _contDef1;
+	KatelloContentDefinition _contDef2;
+	KatelloContentDefinition _contDef3;
+	KatelloContentDefinition compositeContentDef;
 	KatelloContentDefinition compcondef2;
 	KatelloContentView compconview;
 	KatelloContentView compconview2;
@@ -49,46 +49,49 @@ public class CompositeContentViewTests extends KatelloCliTestBase{
 	
 	@Test(description="initialization here")
 	public void init(){
-		condef1 = new KatelloContentDefinition(cli_worker, condef_name1,null, base_org_name, null);
-		exec_result = condef1.create();
+		// create content definition from zoo repo
+		_contDef1 = new KatelloContentDefinition(cli_worker, contDefName1,null, base_org_name, null);
+		exec_result = _contDef1.create();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		exec_result = condef1.add_repo(base_zoo_product_name, base_zoo_repo_name);
+		exec_result = _contDef1.add_repo(base_zoo_product_name, base_zoo_repo_name);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");	
-				
-		exec_result = condef1.publish(pubview_name1_1, pubview_name1_1, "Publish Content");
+		// publish content view zoo - version1
+		exec_result = _contDef1.publish(contViewName1_1, contViewName1_1, "Publish Content");
+		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
+		// publish content view zoo - version2
+		exec_result = _contDef1.publish(contViewName1_2, contViewName1_2, "Publish Content");
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
-		exec_result = condef1.publish(pubview_name1_2, pubview_name1_2, "Publish Content");
+		// create content definition from zoo4 repo
+		_contDef2 = new KatelloContentDefinition(cli_worker, contDefName2,null,base_org_name,null);
+		exec_result = _contDef2.create();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		
-		condef2 = new KatelloContentDefinition(cli_worker, condef_name2,null,base_org_name,null);
-		exec_result = condef2.create();
-		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		exec_result = condef2.add_repo(base_zoo4_product_name, base_zoo4_repo_name);
+		exec_result = _contDef2.add_repo(base_zoo4_product_name, base_zoo4_repo_name);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");	
-				
-		exec_result = condef2.publish(pubview_name2_1, pubview_name2_1, "Publish Content");
+		// publish content view zoo4 - version1
+		exec_result = _contDef2.publish(contViewName2_1, contViewName2_1, "Publish Content");
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		
-		exec_result = condef2.publish(pubview_name2_2, pubview_name2_2, "Publish Content");
+		// publish content view zoo4 - version2
+		exec_result = _contDef2.publish(contViewName2_2, contViewName2_2, "Publish Content");
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 
-		conview1 = new KatelloContentView(cli_worker, pubview_name1_2, base_org_name);
+		// promote both content views' version2 to the DEV environment
+		conview1 = new KatelloContentView(cli_worker, contViewName1_2, base_org_name);
 		exec_result = conview1.promote_view(base_dev_env_name);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		
-		conview2 = new KatelloContentView(cli_worker, pubview_name2_2, base_org_name);
+		conview2 = new KatelloContentView(cli_worker, contViewName2_2, base_org_name);
 		exec_result = conview2.promote_view(base_dev_env_name);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
-		condef3 = new KatelloContentDefinition(cli_worker, condef_name3,null, base_org_name, null);
-		exec_result = condef3.create();
+		// Empty content definition ??? why?
+		_contDef3 = new KatelloContentDefinition(cli_worker, contDefName3,null, base_org_name, null);
+		exec_result = _contDef3.create();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		
-		exec_result = condef3.publish(pubview_name3_1, pubview_name3_1, "Publish Content");
+		// publish content view - version1
+		exec_result = _contDef3.publish(contViewName3_1, contViewName3_1, "Publish Content");
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		
-		exec_result = condef3.publish(pubview_name3_2, pubview_name3_2, "Publish Content");
+		// publish content view - version2
+		exec_result = _contDef3.publish(contViewName3_2, contViewName3_2, "Publish Content");
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 			
 		KatelloUtils.sshOnClient(cli_worker.getClientHostname(),"sed -i -e \"s/certFrequency.*/certFrequency = 1/\" /etc/rhsm/rhsm.conf");
@@ -99,91 +102,91 @@ public class CompositeContentViewTests extends KatelloCliTestBase{
 	
 	@Test(description="Create composite content view definition", dependsOnMethods={"init"})
 	public void test_createComposite() {
-		compcondef = new KatelloContentDefinition(cli_worker, condef_composite_name, null, base_org_name, null);
-		exec_result = compcondef.create(true);
+		compositeContentDef = new KatelloContentDefinition(cli_worker, cdCompositeName, null, base_org_name, null);
+		exec_result = compositeContentDef.create(true);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
-		exec_result = compcondef.add_view(pubview_name1_2);
+		// add zoo content view version2
+		exec_result = compositeContentDef.add_view(contViewName1_2);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");	
-		
-		exec_result = compcondef.add_view(pubview_name2_2);
+		// add zoo4 content view version2
+		exec_result = compositeContentDef.add_view(contViewName2_2);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 	}
 	
-	@Test(description="Check adding old views into composite content view definition", dependsOnMethods={"test_createComposite"})
+	@Test(description="Check adding old views (version 1) into composite content view definition", dependsOnMethods={"test_createComposite"})
 	public void test_checkOldViewsIntoComposite() {
-		exec_result = compcondef.add_view(pubview_name1_1);
+		exec_result = compositeContentDef.add_view(contViewName1_1);
 		Assert.assertTrue(exec_result.getExitCode() == 144, "Check - return code");	
 		Assert.assertTrue(getOutput(exec_result).contains(KatelloContentDefinition.ERR_ADDVIEW), "Error in adding older view");
 		
-		exec_result = compcondef.add_view(pubview_name2_1);
+		exec_result = compositeContentDef.add_view(contViewName2_1);
 		Assert.assertTrue(exec_result.getExitCode() == 144, "Check - return code");
 		Assert.assertTrue(getOutput(exec_result).contains(KatelloContentDefinition.ERR_ADDVIEW), "Error in adding older view");
 	}
 
 	@Test(description="add/remove views into composite content view definition", dependsOnMethods={"test_checkOldViewsIntoComposite"})
 	public void test_addRemoveViewsIntoComposite() {
-		exec_result = compcondef.remove_view(pubview_name1_2);
+		exec_result = compositeContentDef.remove_view(contViewName1_2);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
-		exec_result = compcondef.info();
-		Assert.assertFalse(getOutput(exec_result).contains(pubview_name1_2), "Not contains view");
-		Assert.assertTrue(getOutput(exec_result).contains(pubview_name2_2), "Contains view");
+		exec_result = compositeContentDef.info();
+		Assert.assertFalse(getOutput(exec_result).contains(contViewName1_2), "Not contains view");
+		Assert.assertTrue(getOutput(exec_result).contains(contViewName2_2), "Contains view");
 		
-		exec_result = compcondef.remove_view(pubview_name2_2);
+		exec_result = compositeContentDef.remove_view(contViewName2_2);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
-		exec_result = compcondef.info();
-		Assert.assertFalse(getOutput(exec_result).contains(pubview_name1_2), "Not contains view");
-		Assert.assertFalse(getOutput(exec_result).contains(pubview_name2_2), "Not contains view");
+		exec_result = compositeContentDef.info();
+		Assert.assertFalse(getOutput(exec_result).contains(contViewName1_2), "Not contains view");
+		Assert.assertFalse(getOutput(exec_result).contains(contViewName2_2), "Not contains view");
 		
-		exec_result = compcondef.remove_view(pubview_name2_2);
+		exec_result = compositeContentDef.remove_view(contViewName2_2);
 		Assert.assertTrue(exec_result.getExitCode()==0, "Check exit code (Cannot remove when not a component)");
-		Assert.assertTrue(getOutput(exec_result).equals(String.format(KatelloContentDefinition.ERR_NOT_A_COMPONENT, pubview_name2_2, compcondef.name)), "Check output (not a component)");
+		Assert.assertTrue(getOutput(exec_result).equals(String.format(KatelloContentDefinition.ERR_NOT_A_COMPONENT, contViewName2_2, compositeContentDef.name)), "Check output (not a component)");
 
-		exec_result = compcondef.add_view(pubview_name1_2);
+		exec_result = compositeContentDef.add_view(contViewName1_2);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");	
 
-		exec_result = compcondef.info();
-		Assert.assertTrue(getOutput(exec_result).contains(pubview_name1_2), "Contains view");
-		Assert.assertFalse(getOutput(exec_result).contains(pubview_name2_2), "Not contains view");
+		exec_result = compositeContentDef.info();
+		Assert.assertTrue(getOutput(exec_result).contains(contViewName1_2), "Contains view");
+		Assert.assertFalse(getOutput(exec_result).contains(contViewName2_2), "Not contains view");
 		
-		exec_result = compcondef.add_view(pubview_name2_2);
+		exec_result = compositeContentDef.add_view(contViewName2_2);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
-		exec_result = compcondef.info();
-		Assert.assertTrue(getOutput(exec_result).contains(pubview_name1_2), "Contains view");
-		Assert.assertTrue(getOutput(exec_result).contains(pubview_name2_2), "Contains view");
+		exec_result = compositeContentDef.info();
+		Assert.assertTrue(getOutput(exec_result).contains(contViewName1_2), "Contains view");
+		Assert.assertTrue(getOutput(exec_result).contains(contViewName2_2), "Contains view");
+		// at the end: both version2 of zoo and zoo4 are back in composite content def.
 	}
 	
 	@Test(description="remove view from content definition", dependsOnMethods={"test_consumeCompositeContent"})
 	public void test_removeViewFromDefinition()
 	{
-		String view_name = "new-view"+ KatelloUtils.getUniqueID();    
 		//Remove published view (not a component) - verify error
-		exec_result = compcondef.remove_view(pubcompview_name1);
+		exec_result = compositeContentDef.remove_view(pubcompview_name1);
 		Assert.assertTrue(exec_result.getExitCode()==0, "Check exit code (Cannot remove when not a component)");
-		Assert.assertTrue(getOutput(exec_result).equals(String.format(KatelloContentDefinition.ERR_NOT_A_COMPONENT, pubcompview_name1, compcondef.name)), "Check output (not a component)");
-		//Removed published view (component) - verify that its removed from the Component Views. Present in published view
-		exec_result = compcondef.publish(view_name, view_name, "new View");
-		Assert.assertTrue(exec_result.getExitCode()==0, "Check exit code (publish view)");
-		exec_result = compcondef.add_view(view_name);
+		Assert.assertTrue(getOutput(exec_result).equals(String.format(KatelloContentDefinition.ERR_NOT_A_COMPONENT, pubcompview_name1, compositeContentDef.name)), "Check output (not a component)");
+		// TODO - add here completely NE content view. And then remove it.
+		
+		exec_result = compositeContentDef.add_view(contViewName3_2);
 		Assert.assertTrue(exec_result.getExitCode()==0, "Check exit code (add component view)");
-		exec_result = compcondef.remove_view(view_name);
+		exec_result = compositeContentDef.remove_view(contViewName3_2);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check exit code (remove view)");
-		Assert.assertTrue(getOutput(exec_result).equals(String.format(KatelloContentDefinition.OUT_REMOVE_VIEW, view_name, compcondef.name)), "Check output (remove component)");
+		Assert.assertTrue(getOutput(exec_result).equals(String.format(KatelloContentDefinition.OUT_REMOVE_VIEW, contViewName3_2, compositeContentDef.name)), "Check output (remove component)");
 
-		exec_result = compcondef.info();
+		exec_result = compositeContentDef.info();
 		String viewsList = KatelloUtils.grepCLIOutput("Published Views", getOutput(exec_result).trim());
-		Assert.assertTrue(viewsList.contains(view_name), "Check view name present");
+		Assert.assertTrue(viewsList.contains(contViewName3_2), "Check view name present");
 		viewsList = KatelloUtils.grepCLIOutput("Composite Views", getOutput(exec_result).trim());
-		Assert.assertFalse(viewsList.contains(view_name), "Check view name present");
+		Assert.assertFalse(viewsList.contains(contViewName3_2), "Check view name present");
 		//Try to remove an existing View but not associated with the definition - verify error
-		exec_result = condef2.publish("new-view", "new view", "Publish Content");
+		exec_result = _contDef2.publish("new-view", "new view", "Publish Content");
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		exec_result = compcondef.remove_view("new-view");
+		exec_result = compositeContentDef.remove_view("new-view");
 		Assert.assertTrue(exec_result.getExitCode()==0, "Check exit code (Cannot remove when not a component)");
-		Assert.assertTrue(getOutput(exec_result).equals(String.format(KatelloContentDefinition.ERR_NOT_A_COMPONENT, "new-view", compcondef.name)), "Check output (not a component)");
+		Assert.assertTrue(getOutput(exec_result).equals(String.format(KatelloContentDefinition.ERR_NOT_A_COMPONENT, "new-view", compositeContentDef.name)), "Check output (not a component)");
 	} 
 	
 	@Test(description="Consume content from composite content view definition", dependsOnMethods={"test_addRemoveViewsIntoComposite"})
@@ -191,7 +194,7 @@ public class CompositeContentViewTests extends KatelloCliTestBase{
 		// erase packages
 		KatelloUtils.sshOnClient(cli_worker.getClientHostname(),"yum erase -y wolf lion crab walrus shark cheetah");
 		
-		exec_result = compcondef.publish(pubcompview_name1, pubcompview_name1, "Publish Content");
+		exec_result = compositeContentDef.publish(pubcompview_name1, pubcompview_name1, "Publish Content");
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");		
 		
 		compconview = new KatelloContentView(cli_worker, pubcompview_name1, base_org_name);
@@ -238,7 +241,7 @@ public class CompositeContentViewTests extends KatelloCliTestBase{
 		del_changeset = new KatelloChangeset(cli_worker, del_changeset_name, base_org_name, base_dev_env_name, true);
 		exec_result = del_changeset.create();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
-		exec_result = del_changeset.update_addView(pubview_name1_2);
+		exec_result = del_changeset.update_addView(contViewName1_2);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");		
 		exec_result = del_changeset.apply();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
@@ -251,35 +254,35 @@ public class CompositeContentViewTests extends KatelloCliTestBase{
 	public void test_RePromoteContentViewPart() {
 		KatelloUtils.sshOnClient(cli_worker.getClientHostname(),"yum erase -y tiger");
 		
-		compconview = new KatelloContentView(cli_worker, pubview_name1_2, base_org_name);
+		compconview = new KatelloContentView(cli_worker, contViewName1_2, base_org_name);
 		exec_result = compconview.promote_view(base_dev_env_name);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		Assert.assertTrue(getOutput(exec_result).contains(String.format(
-				KatelloContentView.OUT_PROMOTE, this.pubview_name1_2, base_dev_env_name)), 
+				KatelloContentView.OUT_PROMOTE, this.contViewName1_2, base_dev_env_name)), 
 				"Content view promote output.");
 		install_Packages(cli_worker.getClientHostname(),new String [] {"tiger"});
 	}	
 	
 	@Test(description="Create composite content view definition, add content views, add repos into them, refresh views and try to publish composite view", dependsOnMethods={"init"})
 	public void test_publishCompositeFail() {
-		compcondef2 = new KatelloContentDefinition(cli_worker, condef_composite_name2, null, base_org_name, null);
+		compcondef2 = new KatelloContentDefinition(cli_worker, cdCompositeName2, null, base_org_name, null);
 		exec_result = compcondef2.create(true);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
-		exec_result = compcondef2.add_view(pubview_name3_1);
+		exec_result = compcondef2.add_view(contViewName3_1);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");	
 		
-		exec_result = compcondef2.add_view(pubview_name3_2);
+		exec_result = compcondef2.add_view(contViewName3_2);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
-		exec_result = condef3.add_repo(base_zoo_product_name, base_zoo_repo_name);
+		exec_result = _contDef3.add_repo(base_zoo_product_name, base_zoo_repo_name);
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
-		conview3_1 = new KatelloContentView(cli_worker, pubview_name3_1, base_org_name);
+		conview3_1 = new KatelloContentView(cli_worker, contViewName3_1, base_org_name);
 		exec_result = conview3_1.refresh_view();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
-		conview3_2 = new KatelloContentView(cli_worker, pubview_name3_2, base_org_name);
+		conview3_2 = new KatelloContentView(cli_worker, contViewName3_2, base_org_name);
 		exec_result = conview3_2.refresh_view();
 		Assert.assertTrue(exec_result.getExitCode() == 0, "Check - return code");
 		
