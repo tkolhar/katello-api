db08c6b0e906e655a9b89350c0afa87b3410f23a
Garik Khachikyan <gkhachik@redhat.com>
2013-10-31 12:11:46 +0100

    checkOperation is made as @BeforeSuite

--- src/com/redhat/qe/katello/base/KatelloCliTestBase.java
+++ src/com/redhat/qe/katello/base/KatelloCliTestBase.java
@@ -9,7 +9,7 @@ import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.BeforeClass;
-
+import org.testng.annotations.BeforeSuite;
 import com.redhat.qe.katello.base.obj.KatelloContentDefinition;
 import com.redhat.qe.katello.base.obj.KatelloContentView;
 import com.redhat.qe.katello.base.obj.KatelloEnvironment;
@@ -59,6 +59,29 @@ implements KatelloConstants {
 	protected static String base_pulp_product_id = null;
 	protected static String base_pulp_repo_name = null;
 	protected static String base_pulp_repo_pool = null;
+	
+	@BeforeSuite(alwaysRun=true)
+	public void checkOperational(){
+		String clientHostname = System.getProperty("katello.client.hostname", "localhost");
+		String adminUsername = System.getProperty("katello.admin.user", "admin");
+		String adminPassword = System.getProperty("katello.admin.password", "admin");
+
+		SSHCommandResult exec_result;
+		exec_result = KatelloUtils.sshOnClient(clientHostname, String.format(
+				"rpm -q katello-cli && katello -u%s -p%s ping",adminUsername,adminPassword));
+		if(exec_result.getExitCode().intValue()!=0) 
+			throw new SkipException("Your Katello system seems not operational. Katello CLI ping fails.");
+
+		exec_result = KatelloUtils.sshOnClient(clientHostname, String.format(
+				"rpm -q subscription-manager && subscription-manager orgs --username %s --password %s",adminUsername,adminPassword));
+		if(exec_result.getExitCode().intValue()!=0) 
+			throw new SkipException("Your Katello system seems not operational. RHSM fails.");
+
+		exec_result = KatelloUtils.sshOnClient(clientHostname, 
+				"rpm -q rubygem-hammer_cli rubygem-hammer_cli_foreman && hammer --output base organization list");
+		if(exec_result.getExitCode().intValue()!=0) 
+			throw new SkipException("Your Katello system seems not operational. Hammer CLI ping fails.");
+	}
 	
 	@BeforeClass(alwaysRun=true)
 	public void setUpSuper(){
@@ -372,9 +395,7 @@ implements KatelloConstants {
 			if(!classname.contains("tests.cli.")&&
 				!classname.contains("tests.e2e.")) 
 				return;
-			
-			checkKatelloOperational(cli_worker); // return if system not operational.
-			
+
 			String uid = KatelloUtils.getUniqueID();
 			base_org_name = "CLI Test Org " + uid;
 			base_dev_env_name = "CLI Dev env " + uid;
@@ -467,28 +488,6 @@ implements KatelloConstants {
 			base_zoo4_product_id = prod.custom_getProductId();
 			Assert.assertNotNull(base_zoo4_product_id, "Check - base_zoo_product_id is not null");
 		}	
-	}
-	
-	private static synchronized final void checkKatelloOperational(KatelloCliWorker cli_worker){
-		String clientHostname = cli_worker.getClientHostname();
-		String adminUsername = System.getProperty("katello.admin.user", "admin");
-		String adminPassword = System.getProperty("katello.admin.password", "admin");
-		
-		SSHCommandResult exec_result;
-		exec_result = KatelloUtils.sshOnClient(clientHostname, String.format(
-				"rpm -q katello-cli && katello -u%s -p%s ping",adminUsername,adminPassword));
-		if(exec_result.getExitCode().intValue()!=0) 
-			throw new SkipException("Your Katello system seems not operational. Katello CLI ping fails.");
-		
-		exec_result = KatelloUtils.sshOnClient(clientHostname, String.format(
-				"rpm -q subscription-manager && subscription-manager orgs --username %s --password %s",adminUsername,adminPassword));
-		if(exec_result.getExitCode().intValue()!=0) 
-			throw new SkipException("Your Katello system seems not operational. RHSM fails.");
-
-		exec_result = KatelloUtils.sshOnClient(clientHostname, 
-				"rpm -q rubygem-hammer_cli rubygem-hammer_cli_foreman && hammer --output base organization list");
-		if(exec_result.getExitCode().intValue()!=0) 
-			throw new SkipException("Your Katello system seems not operational. Hammer CLI ping fails.");
 	}
 	
 	protected void promoteEmptyContentView(String org_name, String... env_names) {
